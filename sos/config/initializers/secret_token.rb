98f7946d23159e301fa5f51899edb0119b1eda67
Jonas Tomaz <jonastomazalves@gmail.com>
2013-04-29 12:09:52 -0300

    first commit

---
+++ sos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sos::Application.config.secret_token = '87e1bfe5cded80111545fe767c78ff2bade3371a3a11397c721edfa70a022d48ce3f8f3692fdbc76cd5638207fd89921e98249ffbd189a85ac9558eb5b41e151'
---
+++ sos/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sos::Application.config.session_store :cookie_store, key: '_sos_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sos::Application.config.session_store :active_record_store
